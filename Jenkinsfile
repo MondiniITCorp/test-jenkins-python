49e2c98df5bb0c4126d15368ea54bd48d6f1935e
gsilva-co2hc <g.silva@co2hc.com>
2024-04-18 16:40:52 -0300

    test python pipeline

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,7 +8,7 @@ pipeline {
     }
     stage('hello') {
       steps {
-        sh 'pip3 install python-docx reportlab'
+        sh 'pip3 install python-docx reportlab docx2pdf'
         sh 'python3 hello.py'
       }
     }
--- hello.py
+++ hello.py
@@ -1,54 +1,14 @@
-from docx import Document
-from docx.shared import Inches
-from io import BytesIO
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Image
+import os
+from docx2pdf import convert
 
-def convert_docx_to_pdf(docx_path, pdf_path):
-    # Read the docx file
-    doc = Document(docx_path)
-    
-    # Create a BytesIO object to store the resulting PDF
-    pdf_buffer = BytesIO()
-    
-    # Create a PDF document with reportlab
-    pdf = SimpleDocTemplate(pdf_buffer, pagesize=letter, rightMargin=72, leftMargin=72, topMargin=72, bottomMargin=18)
-    styles = getSampleStyleSheet()
+# Caminho para o arquivo DOCX de entrada
+docx_file = "./CERTIFICADO_DEUDA_AL_DIA_1.docx"
 
-    flowables = []
-    
-    for element in doc.paragraphs:
-        # Handle text paragraphs
-        text = element.text
-        p = Paragraph(text, styles["Normal"])
-        flowables.append(p)
-        
-    # Images are handled differently, they are not directly in the document body
-    for rel in doc.part.rels.values():
-        if "image" in rel.reltype:
-            try:
-                image_data = rel.target_part.blob
-                image_path = "temp_image.png"
-                with open(image_path, 'wb') as img:
-                    img.write(image_data)
-                img = Image(image_path)
-                flowables.append(img)
-            except ValueError:
-                pass  # Skip images with external relationships
-    
-    pdf.build(flowables)
-    
-    # Save the PDF to the specified
-    with open(pdf_path, "wb") as f:
-        f.write(pdf_buffer.getvalue())
+# Caminho para o arquivo PDF de saída
+pdf_file = "output.pdf"
 
-# Path to the input docx file
-docx_file = "CERTIFICADO_DEUDA_AL_DIA_1.docx"
+# Converter o arquivo DOCX para PDF
+convert(docx_file, pdf_file)
 
-# Path to the output pdf file
-pdf_file = "file.pdf"
-
-# Convert the docx file to the pdf file
-convert_docx_to_pdf(docx_file, pdf_file)
+# Definir permissões de acesso para o arquivo PDF (por exemplo, 0o777 para conceder acesso a todos)
+os.chmod(pdf_file, 0o777)
